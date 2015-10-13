8c1c14d286755c1685cdda8cbfa84bcb62870569
Chris Miramontes <nyyankeeschris@gmail.com>
2015-10-12 19:30:51 -0500

    Added deletion of constraints

--- CortanaExtender/MainWindow.xaml.cs
+++ CortanaExtender/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using Windows.Media.SpeechRecognition;
 using Windows.ApplicationModel.VoiceCommands;
 using WindowsInput;
 using System.IO;
+using System.Windows.Forms;
 
 namespace CortanaExtender
 {
@@ -26,13 +27,19 @@ namespace CortanaExtender
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> currentlyStoredConstraints;
         private List<string> constraints;
         private SpeechRecognizer backgroundListener;
         private TypedEventHandler<SpeechContinuousRecognitionSession, SpeechContinuousRecognitionResultGeneratedEventArgs> BLResultGenerated;
+        private NotifyIcon notifyIcon;
+        private string filePath;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            string fileName = @"Stored_Constraints.txt";
+            filePath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), fileName);
 
             backgroundListener = new SpeechRecognizer();
             constraints = new List<string>();
@@ -40,16 +47,19 @@ namespace CortanaExtender
             backgroundListener.ContinuousRecognitionSession.ResultGenerated += BLResultGenerated;
 
             constraints = readInConstraintsFromFile();
+            currentlyStoredConstraints = constraints.ToList();
             updateConstraintsWindow(constraints);
+
+            this.Closing += OnAppClosing;
 
             var waitOn = loadOnStart();
             while (waitOn.Status != AsyncStatus.Completed) { }
             var ff = backgroundListener.ContinuousRecognitionSession.StartAsync();
 
-            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
-            ni.Icon = new System.Drawing.Icon("trayImage.ico");
-            ni.Visible = true;
-            ni.DoubleClick +=
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new System.Drawing.Icon("trayImage.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick +=
                 delegate (object sender, EventArgs args)
                 {
                     this.Show();
@@ -59,10 +69,6 @@ namespace CortanaExtender
 
         private List<string> readInConstraintsFromFile()
         {
-            string fileName = @"Stored_Constraints.txt";
-            string currPath = Directory.GetCurrentDirectory();
-            string filePath = System.IO.Path.Combine(currPath, fileName);
-
             List<string> existingConstraints = new List<string>();
             string line;
 
@@ -73,11 +79,12 @@ namespace CortanaExtender
                     File.Create(filePath);
                 }
 
-                System.IO.StreamReader file1 = new System.IO.StreamReader(filePath);
-                while ((line = file1.ReadLine()) != null)
+                StreamReader reader = new StreamReader(filePath);
+                while ((line = reader.ReadLine()) != null)
                 {
                     existingConstraints.Add(line);
                 }
+                reader.Close();
             }
             catch (Exception ex)
             {
@@ -85,6 +92,26 @@ namespace CortanaExtender
             }
 
             return existingConstraints;
+        }
+
+        private void WriteConstraintsToFile()
+        {
+            IEnumerable<string> NewConstraintsToSave = constraints.Except(currentlyStoredConstraints);
+
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                foreach (string str in NewConstraintsToSave)
+                {
+                    writer.WriteLine(str);
+                }
+                writer.Close();
+            }
+        }
+
+        private void OnAppClosing(Object sender, EventArgs args)
+        {
+            notifyIcon.Visible = false;
+            WriteConstraintsToFile();
         }
 
         private void blResultGenerated(SpeechContinuousRecognitionSession session, SpeechContinuousRecognitionResultGeneratedEventArgs args)
@@ -97,18 +124,62 @@ namespace CortanaExtender
 
         private void Add_Constraint(Object sender, RoutedEventArgs e)
         {
-            if (backgroundListener.State != SpeechRecognizerState.Idle && backgroundListener.State != SpeechRecognizerState.Paused)
+            if (customPhrase.Text != "" && customPhrase.Text != null)
             {
-                var async = backgroundListener.ContinuousRecognitionSession.StopAsync();
-                while (async.Status != AsyncStatus.Completed) { }
+                if (backgroundListener.State != SpeechRecognizerState.Idle && backgroundListener.State != SpeechRecognizerState.Paused)
+                {
+                    var async = backgroundListener.ContinuousRecognitionSession.StopAsync();
+                    while (async.Status != AsyncStatus.Completed) { }
+                }
+
+                constraints.Add(customPhrase.Text);
+                updateConstraintsWindow(constraints);
+                backgroundListener.Constraints.Clear();
+                backgroundListener.Constraints.Add(new SpeechRecognitionListConstraint(constraints));
+                var blCompileConstraints = backgroundListener.CompileConstraintsAsync();
+                while (blCompileConstraints.Status != AsyncStatus.Completed) { }
+                var leavemealone = backgroundListener.ContinuousRecognitionSession.StartAsync();
             }
-            constraints.Add(customPhrase.Text);
-            updateConstraintsWindow(constraints);
-            backgroundListener.Constraints.Clear();
-            backgroundListener.Constraints.Add(new SpeechRecognitionListConstraint(constraints));
-            var blCompileConstraints = backgroundListener.CompileConstraintsAsync();
-            while (blCompileConstraints.Status != AsyncStatus.Completed) { }
-            var leavemealone = backgroundListener.ContinuousRecognitionSession.StartAsync();
+        }
+
+        private void Remove_Constraint(Object sender, RoutedEventArgs e)
+        {
+            if (removePhrase.Text != "" && removePhrase.Text != null)
+            {
+                if (backgroundListener.State != SpeechRecognizerState.Idle && backgroundListener.State != SpeechRecognizerState.Paused)
+                {
+                    var async = backgroundListener.ContinuousRecognitionSession.StopAsync();
+                    while (async.Status != AsyncStatus.Completed) { }
+                }
+
+                if (constraints.Remove(removePhrase.Text))
+                {
+                    DeleteConstraintFromFile(removePhrase.Text);
+                }
+
+                updateConstraintsWindow(constraints);
+                removePhrase.Text = string.Empty;
+                backgroundListener.Constraints.Clear();
+                backgroundListener.Constraints.Add(new SpeechRecognitionListConstraint(constraints));
+                var blCompileConstraints = backgroundListener.CompileConstraintsAsync();
+                while (blCompileConstraints.Status != AsyncStatus.Completed) { }
+                var useless = backgroundListener.ContinuousRecognitionSession.StartAsync();
+            }
+        }
+
+        private void DeleteConstraintFromFile(string toDelete)
+        {
+            StringBuilder builder = new StringBuilder();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line = "";
+                while ((line = reader.ReadLine()) != null && line != toDelete)
+                {
+                    builder.AppendLine(line);
+                }
+                reader.Close();
+            }
+            File.WriteAllText(filePath, builder.ToString());
         }
 
         private void updateConstraintsWindow(List<string> cons)
@@ -120,14 +191,6 @@ namespace CortanaExtender
             }
 
             TextBlockSavedConstraints.Text = builder.ToString();
-        }
-
-        protected override void OnStateChanged(EventArgs e)
-        {
-            if (WindowState == System.Windows.WindowState.Minimized)
-                this.Hide();
-
-            base.OnStateChanged(e);
         }
 
         private IAsyncOperation<SpeechRecognitionCompilationResult> loadOnStart()
